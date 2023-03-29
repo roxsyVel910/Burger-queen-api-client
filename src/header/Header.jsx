7a281c3bc910cc3099d0f7ad6607c0160c3e66d8
Roxsy <roxsy.dj@gmail.com>
2023-03-29 07:40:53 -0500

    creacion de componente de header

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,2 +1,21 @@
+import { AppBar, Avatar, Toolbar, Typography } from "@mui/material";
 
+
+function Header() {
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          {/* Logo de la empresa a la izquierda */}
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+           Burger Quien
+          </Typography>
+  
+          {/* Avatar a la derecha */}
+          <Avatar alt="Mi Avatar" src="/ruta/al/avatar.jpg" />
+        </Toolbar>
+      </AppBar>
+    );
+  }
+  
+  export default Header;
   
